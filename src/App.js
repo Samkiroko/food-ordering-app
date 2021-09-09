0b486e716bf21ab58ad891465c3cf4e95c7284bb
Samkiroko <kirokodev@gmail.com>
2021-09-09 21:48:32 +0300

    Adding an input cart and modal

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import { Fragment } from 'react'
+import { Fragment, useState } from 'react'
+import Cart from './components/Cart/Cart'
 import Header from './components/Layout/Header'
 import Meals from './components/Meals/Meals'
 
 function App() {
+  const [cartIsShow, setCartIsShow] = useState(false)
+
+  const showCartHandler = () => {
+    setCartIsShow(!cartIsShow)
+  }
   return (
     <Fragment>
-      <Header />
+      {cartIsShow && <Cart onClose={showCartHandler} />}
+      <Header onShowCart={showCartHandler} />
+
       <main>
         <Meals />
       </main>
